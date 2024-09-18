c0fec9a735cc6aafe8aa07678b46a8aba1e6eba1
fulanoDoGithub <contaforaula@gmail.com>
2024-09-18 18:21:05 -0300

    Create tutorial.py

---
+++ 17-09/tutorial.py
@@ -0,0 +1,16 @@
+# EXERCÍCIO 01
+
+user_age = int(input("Quantos anos você tem? "))
+# condicional que coisa tal coisa
+if user_age <- 0:
+  print("Você não é nem projeto ainda.")
+elif user_age <= 12:
+  print("Criança")
+elif user_age <= 17:
+  print("Adolescente")
+elif user_age <= 64:
+  print("Adulto")
+else:
+  print("Idoso")
+
+# --------------------------------------------------------------
